23c3ed0bff36f7ccc634a142438051aed8131baf
brentcrosby <brentobox241@gmail.com>
2025-10-21 17:39:07 -0700

    update placeholders

--- src/constants/titlePlaceholders.js
+++ src/constants/titlePlaceholders.js
@@ -1,9 +1,9 @@
 export const TITLE_PLACEHOLDERS = [
+  "Organize tasks 5m",
   "Study 25m 5m 25m",
   "Code review 45:00",
   "Deep work 2h",
   "Read book 1:30:00",
   "Watch lecture 1h30m",
-  "Practice guitar 15:00 15:00",
-  "Do the dishes 10m"
+  "Practice guitar 15:00 15:00"
 ];
