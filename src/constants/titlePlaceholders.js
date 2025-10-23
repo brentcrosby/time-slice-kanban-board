997346ce298dc06f09a2f9e7570bbd787c83eaf2
Brent Crosby <heybrentcrosby@gmail.com>
2025-10-23 10:50:08 -0700

    update placeholder text to include group shorthand

--- src/constants/titlePlaceholders.js
+++ src/constants/titlePlaceholders.js
@@ -1,9 +1,9 @@
 export const TITLE_PLACEHOLDERS = [
   "Organize tasks 5m",
-  "Study 25m 5m 25m",
+  "Study 25m 5m 25m g1",
   "Code review 45:00",
-  "Deep work 2h",
+  "Deep work 2h g2",
   "Read book 1:30:00",
-  "Watch lecture 1h30m",
+  "Watch lecture 1h30m g3",
   "Practice guitar 15:00 15:00"
 ];
