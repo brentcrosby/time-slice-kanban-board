8d20e5228bfa4beaf5a5037aaf01585eca43a6c5
Brent Crosby <heybrentcrosby@gmail.com>
2026-09-25 01:23:23 -0700

    Bust browser cache for Tasky favicon

--- scripts/generate-icons.mjs
+++ scripts/generate-icons.mjs
@@ -6,7 +6,7 @@ import { Resvg } from "@resvg/resvg-js";
 const publicDir = join(dirname(fileURLToPath(import.meta.url)), "..", "public");
 
 for (const [source, output, size] of [
-  ["favicon.svg", "favicon-32.png", 32],
+  ["favicon-kanban.svg", "favicon-kanban-32.png", 32],
   ["app-icon.svg", "apple-touch-icon.png", 180],
   ["app-icon.svg", "icon-192.png", 192],
   ["app-icon.svg", "icon-512.png", 512],
